80be00e0a69cdd9c61e3e4f0d88233c16ba8ba42
Samuel Panek <samuelrpanek@gmail.com>
2021-09-21 16:41:16 -0700

    changes some formatting suggested by Nathan

--- app.js
+++ app.js
@@ -28,79 +28,86 @@ let grandestTotal =0;
 let times =[sixAm,sevenAm,eightAm,nineAm,tenAm,elevenAm,twelvePm,onePm,twoPm,threePm,fourPm,fivePm,sixPm,sevenPm,grandestTotal];
 
 function CityLocation (city, minCust, maxCust, avgCookieSale){
-  this.city = city;
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-  this.avgCookieSale = avgCookieSale;
+	this.city = city;
+	this.minCust = minCust;
+	this.maxCust = maxCust;
+	this.avgCookieSale = avgCookieSale;
 }
 CityLocation.prototype.salesPerHour =function(){
-  let citySalesDay = [];
-  let daySales = 0;
-  for (let hours = 0; hours<time.length; hours++){
-    let citySalesPH = Math.floor((this.minCust+ Math.floor(Math.random() * ((this.maxCust - this.minCust) + 1)))* this.avgCookieSale);
-    times[hours] = times[hours] + citySalesPH;
-    citySalesDay.push(citySalesPH);
-    daySales = daySales + citySalesPH;
-  }
-  times[time.length]=times[time.length] + daySales;
-  citySalesDay[time.length] = daySales;
-  console.log(citySalesDay);
-  return citySalesDay;
+	let citySalesDay = [];
+	let daySales = 0;
+
+	for (let hours = 0; hours<time.length; hours++){
+		let citySalesPH = Math.floor((this.minCust+ Math.floor(Math.random() * ((this.maxCust - this.minCust) + 1)))* this.avgCookieSale);
+		times[hours] = times[hours] + citySalesPH;
+		citySalesDay.push(citySalesPH);
+		daySales = daySales + citySalesPH;
+	}
+
+	times[time.length]=times[time.length] + daySales;
+	citySalesDay[time.length] = daySales;
+	console.log(citySalesDay);
+	return citySalesDay;
 };
 
 CityLocation.prototype.salesHeader = function(){
-  let salesFigures = document.getElementById('sales-figures');
-  let tableRow = document.createElement('tr');
-  salesFigures.appendChild(tableRow);
-  let tableCity = document.createElement('td');
-  tableCity.textContent ='';
-  tableRow.appendChild(tableCity);
-  for (let hours = 0; hours <time.length; hours++){
-    let tableCell= document.createElement('td');
-    tableCell.textContent =time[hours];
-    tableRow.appendChild(tableCell);
-  }
-  let columnTotal = document.createElement('td');
-  columnTotal.textContent = 'Daily Locatioin Totals';
-  tableRow.appendChild(columnTotal);
+	let salesFigures = document.getElementById('sales-figures');
+	let tableRow = document.createElement('tr');
+	let tableCity = document.createElement('td');
+
+	salesFigures.appendChild(tableRow);
+	tableCity.textContent ='';
+	tableRow.appendChild(tableCity);
+
+	for (let hours = 0; hours <time.length; hours++){
+		let tableCell= document.createElement('td');
+		tableCell.textContent =time[hours];
+		tableRow.appendChild(tableCell);
+	}
+
+	let columnTotal = document.createElement('td');
+	columnTotal.textContent = 'Daily Locatioin Totals';
+	tableRow.appendChild(columnTotal);
 };
+
 CityLocation.prototype.salesTable = function(){
-  let saleVar =this.salesPerHour();
-  let salesFigures = document.getElementById('sales-figures');
-  let tableRow = document.createElement('tr');
-  salesFigures.appendChild(tableRow);
-  let tableCity = document.createElement('td');
-  tableCity.textContent = this.city;
-  tableRow.appendChild(tableCity);
-  for (let hours = 0; hours <time.length; hours++){
-    let tableCell= document.createElement('td');
-    tableCell.textContent =time[hours] + ' Total:' + saleVar[hours];
-    tableRow.appendChild(tableCell);
-  }
-  let totalCell = document.createElement('td');
-  totalCell.textContent ='Daily Total:' + saleVar[time.length];
-  tableRow.appendChild(totalCell);
+	let saleVar =this.salesPerHour();
+	let salesFigures = document.getElementById('sales-figures');
+	let tableRow = document.createElement('tr');
+	salesFigures.appendChild(tableRow);
+	let tableCity = document.createElement('td');
+	tableCity.textContent = this.city;
+	tableRow.appendChild(tableCity);
+
+	for (let hours = 0; hours <time.length; hours++){
+		let tableCell= document.createElement('td');
+		tableCell.textContent =time[hours] + ' Total:' + saleVar[hours];
+		tableRow.appendChild(tableCell);
+	}
+
+	let totalCell = document.createElement('td');
+	totalCell.textContent ='Daily Total:' + saleVar[time.length];
+	tableRow.appendChild(totalCell);
 };
 
 CityLocation.prototype.salesFooter = function(){
-  let salesFigures = document.getElementById('sales-figures');
-  let tableRow = document.createElement('tr');
-  salesFigures.appendChild(tableRow);
-  let totalRow = document.createElement('td');
-  totalRow.textContent = 'Global Totals:';
-  tableRow.appendChild(totalRow);
-  for (let hours = 0; hours <time.length; hours++){
-    let tableCell= document.createElement('td');
-    tableCell.textContent ='Global ' + time[hours] + ' Total:' + times[hours];
-    tableRow.appendChild(tableCell);
-  }
-  let totalCell = document.createElement('td');
-  totalCell.textContent ='Global Day Total:' + times[time.length];
-  tableRow.appendChild(totalCell);
+	let salesFigures = document.getElementById('sales-figures');
+	let tableRow = document.createElement('tr');
+	salesFigures.appendChild(tableRow);
+	let totalRow = document.createElement('td');
+	totalRow.textContent = 'Global Totals:';
+	tableRow.appendChild(totalRow);
 
+	for (let hours = 0; hours <time.length; hours++){
+		let tableCell= document.createElement('td');
+		tableCell.textContent ='Global ' + time[hours] + ' Total:' + times[hours];
+		tableRow.appendChild(tableCell);
+	}
+
+	let totalCell = document.createElement('td');
+	totalCell.textContent ='Global Day Total:' + times[time.length];
+	tableRow.appendChild(totalCell);
 };
-
-
 
 let seattleTotal = new CityLocation(globalLocation[0][0],globalLocation[0][1],globalLocation[0][2],globalLocation[0][3]);
 let tokyoTotal = new CityLocation(globalLocation[1][0],globalLocation[1][1],globalLocation[1][2],globalLocation[1][3]);
